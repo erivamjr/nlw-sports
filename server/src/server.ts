98ff8fd6454d7f4ae76ce3ba34d39bf5ff4933f9
José Erivam <erivamdev@gmail.com>
2022-09-14 21:29:59 -0300

    :sparkles: feat:add prisma

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,13 +2,22 @@ import express from "express";
 
 const app = express();
 
-app.get("/ads", (req, res) => {
-  return res.json([
-    { id: 1, name: "Anuncio 1" },
-    { id: 2, name: "Anuncio 2" },
-    { id: 3, name: "Anuncio 3" },
-    { id: 3, name: "Anuncio 4" },
-  ]);
+app.get('/games', (req, res) => {
+  return res.status(200).json([])
+});
+
+app.post('/ads', (req, res) => {
+  return res.status(201).json([])
+});
+
+const mock = [
+  { id: 1, name: "Anuncio 1" },
+  { id: 2, name: "Anuncio 2" },
+  { id: 3, name: "Anuncio 3" },
+  { id: 3, name: "Anuncio 4" },
+]
+app.get('/games/:id/ads', (req, res) => {
+  return res.status(200).json(mock);
 });
 
 app.listen(3001, () => {
