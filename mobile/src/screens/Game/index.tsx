7f55365cddd72944493c77076d2420af56e28068
José Erivam <erivamdev@gmail.com>
2022-09-16 19:59:45 -0300

    :sparkles: feat:add details game

---
+++ mobile/src/screens/Game/index.tsx
@@ -0,0 +1,48 @@
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { Background } from '../../components/Background';
+import { useRoute } from '@react-navigation/native';
+import { Entypo } from '@expo/vector-icons';
+
+import logoImg from '../../assets/logo-nlw-esports.png';
+import { styles } from './styles';
+import { GameParams } from '../../@types/navigation';
+import { TouchableOpacity, View, Image } from 'react-native';
+import { THEME } from '../../theme';
+import React from 'react';
+import { Heading } from '../../components/Heading';
+
+
+export function Game() {
+
+  const route = useRoute();
+  const game = route.params as GameParams;
+
+  return (
+    <Background>
+
+      < SafeAreaView style={styles.container}>
+        <View style={styles.header}>
+          <TouchableOpacity>
+            <Entypo
+              name="chevron-thin-left"
+              color={THEME.COLORS.CAPTION_300}
+              size={20}
+            />
+          </TouchableOpacity>
+
+          <Image
+            source={logoImg}
+            style={styles.logo}
+          />
+          <View style={styles.right} />
+        </View>
+
+        <Heading
+          title={game.title}
+          subtitle={"Conecte-se e comece a jogar!"}
+        />
+
+      </ SafeAreaView >
+    </Background>
+  );
+}
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,42 +1,56 @@
 import { useEffect, useState } from 'react'
-import { View, Image, FlatList } from 'react-native';
+import { Image, FlatList } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { useNavigation } from '@react-navigation/native';
 
 import { styles } from './styles';
 
 import logoImg from '../../assets/logo-nlw-esports.png';
 import { Heading } from '../../components/Heading';
 import { GameCard, GameCardProps } from '../../components/GameCard';
+import { Background } from '../../components/Background';
 
 export function Home() {
   const [games, setGames] = useState<GameCardProps[]>([]);
 
+  const navigation = useNavigation();
+  function handleOpenGame({ id, title, bannerUrl }: GameCardProps) {
+    navigation.navigate('game', { id, title, bannerUrl });
+  }
+
   useEffect(() => {
-    fetch('http://172.19.0.1:3001/games')
+    fetch('http://192.168.1.12:3001/games')
       .then(response => response.json())
       .then(data => setGames(data));
   }, []);
 
 
   return (
-    <View style={styles.container}>
-      <Image source={logoImg}
-        style={styles.logo} />
+    <Background>
 
-      <Heading
-        title="Encontre seu duo!"
-        subtitle="Selecione o que deseja jogar..."
-      />
+      <SafeAreaView style={styles.container}>
+        <Image
+          source={logoImg}
+          style={styles.logo} />
 
-      <FlatList
-        data={games}
-        keyExtractor={item => item.id}
-        renderItem={({ item }) => (
-          <GameCard data={item} />
-        )}
-        showsHorizontalScrollIndicator={false}
-        horizontal
-        contentContainerStyle={styles.contentList}
-      />
-    </View>
+        <Heading
+          title="Encontre seu duo!"
+          subtitle="Selecione o que deseja jogar..."
+        />
+
+        <FlatList
+          data={games}
+          keyExtractor={item => item.id}
+          renderItem={({ item }) => (
+            <GameCard
+              data={item}
+              onPress={() => handleOpenGame(item)} />
+          )}
+          showsHorizontalScrollIndicator={false}
+          horizontal
+          contentContainerStyle={styles.contentList}
+        />
+      </SafeAreaView >
+    </Background>
   );
 }
